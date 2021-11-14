23bf2c49003d6a529cfa497abc145690802dc16f
vasantha <vasantha555@gmail.com>
2021-11-13 23:30:40 -0600

    modified classes

--- src/main/java/resources/Browser.java
+++ src/main/java/resources/Browser.java
@@ -1,15 +1,32 @@
 package resources;
 
+import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class Browser {
     WebDriver driver;
 
-    public WebDriver initializeBrowser() {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\chand\\IdeaProjects\\MavenProject\\Drivers\\chromedriver.exe");
-        driver = new ChromeDriver();
-        driver.get("https://www.saucedemo.com");
+    public WebDriver initializeBrowser() throws MalformedURLException {
+//        System.setProperty("webdriver.chrome.driver", "C:\\Users\\chand\\IdeaProjects\\MavenProject\\Drivers\\chromedriver.exe");
+//        driver = new ChromeDriver();
+//        driver.get("https://www.saucedemo.com");
+
+        MutableCapabilities sauceOptions=new MutableCapabilities();
+        sauceOptions.setCapability("name","Vasantha Login Test");
+        ChromeOptions browserOptions = new ChromeOptions();
+        browserOptions.setCapability("platformName", "Windows 10");
+        browserOptions.setCapability("browserVersion", "latest");
+        browserOptions.setCapability("sauce:options", sauceOptions);
+
+
+
+        driver = new RemoteWebDriver(new URL("https://oauth-vasantha555-15d75:1bc4cef7-faba-4971-8a32-363d01119162@ondemand.us-west-1.saucelabs.com:443/wd/hub"),browserOptions);
         return driver;
     }
 }
--- src/test/java/StepDefinitions/LoginStepDefinition.java
+++ src/test/java/StepDefinitions/LoginStepDefinition.java
@@ -6,11 +6,19 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import pages.LoginPage;
 import resources.Browser;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 import static java.lang.System.*;
 
@@ -18,11 +26,20 @@ public class LoginStepDefinition extends Browser {
 
     WebDriver driver;
 
+    {
+        driver = initializeBrowser();
+    }
+
+    public LoginStepDefinition() throws MalformedURLException {
+    }
+//        System.setProperty("webdriver.chrome.driver", "C:\\Users\\chand\\IdeaProjects\\MavenProject\\Drivers\\chromedriver.exe");
+//        driver = new ChromeDriver();
+
 
     @Given("Navigate to url")
-    public void OpeningUrl()
-    {
-        driver=initializeBrowser();
+    public void OpeningUrl() throws MalformedURLException {
+
+        driver.get("https://www.saucedemo.com");
 
     }
 
